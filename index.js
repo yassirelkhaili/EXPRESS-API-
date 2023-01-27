c4f86bcc5475237e10e45348fddc4d6d2c9ed198
yassirelkhaili <elkhailiyassir@gmail.com>
2023-01-27 01:20:22 +0100

    initial commit

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ app.listen(port, () => {
   console.log(`server listening on port: ${port}`);
 });
 
+const db = mysql.createConnection({
+  host: "sql203.epizy.com",
+  port: "3306",
+  user: "epiz_33125955",
+  password: "Pbkt1UoJphUl",
+  database: "epiz_33125955_expresscrud",
+});
+
+app.post("/", (req, res) => {
+  const { fname, lname, age, pos, email } = req.body;
+  const values = [fname, lname, age, pos, email];
+  const query =
+    "INSERT INTO employees (fname, lname, age, pos, email) VALUES (?)";
+  db.query(query, [values], (err, data) => {
+    if (err) {
+      res.status(404).send(err);
+    } else res.status(200).send("hakuna matata");
+  });
+});
