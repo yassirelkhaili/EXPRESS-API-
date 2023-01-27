4ee3ec9408eb6216a165edeee3b0fd69f4c71911
yassirelkhaili <elkhailiyassir@gmail.com>
2023-01-27 16:51:45 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import data from "./data.js";
+import mysql from "mysql2";
+
 
 const app = express();
 const port = 5000;
@@ -12,11 +13,25 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
+const db = mysql.createConnection({
+  host: "sql203.epizy.com",
+  user: "epiz_33125955",
+  password: "Pbkt1UoJphUl",
+  database: "epiz_33125955_expresscrud"
+});
+
 app.listen(port, () => {
   console.log(`server listening on port: ${port}`);
 });
 
 app.get("/", (req,res) => {
-  res.json(data);
+  const query = "SELECT * from employees";
+  db.query(query, (err, data) => {
+    if (err) {
+      res.status(404).json(err);
+    } else {
+      res.status(200).json(data)
+    }
+  })
 })
 
